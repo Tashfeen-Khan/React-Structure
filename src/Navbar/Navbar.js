1cf1a1165476ea4410bf1833b42efba92bdbea17
tashfeenkhan158@gmail.com <tashfeenkhan158@gmail.com>
2024-02-02 17:30:58 +0500

    today changes

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -6,14 +6,14 @@ const Navbar = () => {
   return (
     <header class="text-gray-600 body-font">
       <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-          <img
-            class="w-16 h-16 text-white p-2  rounded-full"
-            src={logo}
-            alt="logo img"
-          />
-          <span class="ml-3 text-xl">Tashfeen Khan</span>
-        </a>
+        {/* <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"> */}
+        <img
+          class="w-16 h-16 text-white p-2  rounded-full"
+          src={logo}
+          alt="logo img"
+        />
+        <span class="ml-3 text-xl">Tashfeen Khan</span>
+        {/* </a> */}
         <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
           <Link to="/" class="mr-5 hover:text-gray-900 font-bold text-2xl">
             Home
